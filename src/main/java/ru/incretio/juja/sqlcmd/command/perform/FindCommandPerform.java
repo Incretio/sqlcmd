73ba75aeb3c6409dc7b0359010c25b62cc4803fe
incretio <incretio@gmail.com>
2017-02-07 20:54:32 +0300

    Реализовано примитивное выполнение команды find

--- src/main/java/ru/incretio/juja/sqlcmd/command/perform/FindCommandPerform.java
+++ src/main/java/ru/incretio/juja/sqlcmd/command/perform/FindCommandPerform.java
@@ -3,6 +3,8 @@ package ru.incretio.juja.sqlcmd.command.perform;
 import ru.incretio.juja.sqlcmd.command.Command;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindCommandPerform implements Command {
@@ -16,20 +18,56 @@ public class FindCommandPerform implements Command {
             ResultSet resultSet = statement.executeQuery(
                     "SELECT * FROM public.\"" + tableName + "\"");
             ResultSetMetaData metaData = resultSet.getMetaData();
-            result = "";
-            for (int i = 0; i < metaData.getColumnCount(); i++) {
-                result += metaData.getColumnLabel(i + 1) + "\t";
+
+            for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
+                result += String.format("+-%" + (metaData.getColumnDisplaySize(i)) + "s", "").replace(" ", "-");
             }
-            result += "\n";
+            result += "+\n";
+            for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
+                result += String.format("+ %s%" + (metaData.getColumnDisplaySize(i) - metaData.getColumnLabel(i).length()) + "s", metaData.getColumnLabel(i), "");
+            }
+            result += "+\n";
+            for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
+                result += String.format("+-%" + (metaData.getColumnDisplaySize(i)) + "s", "").replace(" ", "-");
+            }
+            result += "+\n";
+
             while (resultSet.next()) {
-                result += resultSet.getString(1) + "\t" + resultSet.getString(2) + "\n";
+                for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
+                    int columnSize = metaData.getColumnDisplaySize(i);
+                    int valueLength = (resultSet.getString(i) == null) ? 0 : resultSet.getString(i).length();
+                    String value = (resultSet.getString(i) == null) ? "" : resultSet.getString(i);
+                    result += String.format("+ %s%" + (columnSize - valueLength) + "s", value, "");
+                }
+                result += "+\n";
             }
+
+            for (int i = 1; i < metaData.getColumnCount() + 1; i++) {
+                result += String.format("+-%" + (metaData.getColumnDisplaySize(i)) + "s", "").replace(" ", "-");
+            }
+            result += "+\n";
+
+
             result = (result.trim().isEmpty()) ? "В таблице " + tableName + " отсутствуют данные." : result;
         } catch (SQLException e) {
             result = e.getMessage();
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> list = new ArrayList<>();
+        list.add(Arrays.asList(new String[]{"123", "stiven", "pupkin"}));
+        list.add(Arrays.asList(new String[]{"456", "stiv", "pupk"}));
+        list.add(Arrays.asList(new String[]{"789", "sti", "pup"}));
+        System.out.print("+");
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(String.format(" %s%" + (10 - list.get(i).get(j).length()) + "s", list.get(i).get(j), "+"));
+
+            }
+        }
     }
 
 }
