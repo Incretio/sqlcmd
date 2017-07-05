bce12878c639cd00ba9d6918e64a0a0b8ad1de69
incretio <incretio@gmail.com>
2017-07-05 23:10:18 +0300

    Перенёс из Action: clear, help, menu и несколько системных методов.

--- src/main/java/ru/incretio/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ru/incretio/juja/sqlcmd/controller/MainController.java
@@ -1,13 +1,75 @@
 package ru.incretio.juja.sqlcmd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.incretio.juja.sqlcmd.service.Service;
+import ru.incretio.juja.sqlcmd.service.ServiceFactory;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
+
+    private Service service;
+    @Autowired
+    private ServiceFactory serviceFactory;
+
+    @RequestMapping(value = {"/", "/menu"}, method = RequestMethod.GET)
+    public String menu(HttpServletRequest request) {
+        configureRequest(request);
+        return "menu";
+    }
+
     @RequestMapping(value = "/help", method = RequestMethod.GET)
-    public String help() {
+    public String help(HttpServletRequest request) {
+        configureRequest(request);
         return "help";
+    }
+
+    @RequestMapping(value = "/clear", method = RequestMethod.GET)
+    public String clearGet(HttpServletRequest request) {
+        configureRequest(request);
+        return "clear";
+    }
+
+    @RequestMapping(value = "/clear", method = RequestMethod.POST)
+    public String clearPost(HttpServletRequest request) {
+        String tableName = request.getParameter("tableName");
+        try {
+            String message = service.clear(tableName);
+            return openMenuPage(request, message);
+        } catch (Exception e) {
+            return toProcessServiceException(request, e);
+        }
+    }
+
+    private void configureRequest(HttpServletRequest req) {
+        updateService(req);
+        setMenuToAttribute(req);
+    }
+
+    protected void updateService(HttpServletRequest req) {
+        Service sessionService = (Service) req.getSession().getAttribute("service");
+        if (sessionService != null) {
+            service = sessionService;
+        } else {
+            service = serviceFactory.makeService();
+        }
+    }
+
+    private void setMenuToAttribute(HttpServletRequest request) {
+        request.setAttribute("items", service.commandsList());
+    }
+
+    protected String openMenuPage(HttpServletRequest request, String message) {
+        request.setAttribute("message", message);
+        return "redirect:/menu";
+    }
+
+    protected String toProcessServiceException(HttpServletRequest request, Exception e) {
+        request.setAttribute("message", e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+        return "errorPage";
     }
 }
