810d4ae80f5c6052262098083dfdf1b00b18ec84
incretio <incretio@gmail.com>
2017-07-05 23:09:24 +0300

    Изменена версия spring и вернул обратно @Lookup для ServiceFactory

--- src/main/java/ru/incretio/juja/sqlcmd/service/ServiceFactory.java
+++ src/main/java/ru/incretio/juja/sqlcmd/service/ServiceFactory.java
@@ -1,9 +1,11 @@
 package ru.incretio.juja.sqlcmd.service;
 
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Component;
 
 @Component
 public abstract class ServiceFactory {
 
+    @Lookup
     public abstract Service makeService();
 }
